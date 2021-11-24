270b3f7d52ee60f04d757378974041257f9a9ade
Erick Freitas <erickdefg@gmail.com>
2021-11-23 22:09:13 -0300

    create hook Users

--- src/hooks/useCategory.ts
+++ src/hooks/useCategory.ts
@@ -6,13 +6,17 @@ import { CarCategory } from 'interfaces/cars';
 import categoryService from 'services/CategoryService';
 
 export const useGetCategories = (options?: UseQueryOptions) => {
-  const { data: items, ...rest } = useQuery(['Cars'], categoryService.getAll, {
-    staleTime: 1000 * 60 * 5, // 5 minutes
-    onError: (error: AxiosError) => {
-      toast.error(error.message);
-    },
-    ...options,
-  });
+  const { data: items, ...rest } = useQuery(
+    ['Categories'],
+    categoryService.getAll,
+    {
+      staleTime: 1000 * 60 * 5, // 5 minutes
+      onError: (error: AxiosError) => {
+        toast.error(error.message);
+      },
+      ...options,
+    }
+  );
   const data = items as CarCategory;
   return { data, ...rest };
 };
--- src/hooks/useUsers.ts
+++ src/hooks/useUsers.ts
@@ -2,17 +2,17 @@ import { useQuery, UseQueryOptions } from 'react-query';
 import { toast } from 'react-toastify';
 
 import { AxiosError } from 'axios';
-import { CarCategory } from 'interfaces/cars';
-import categoryService from 'services/CategoryService';
+import { IUser } from 'interfaces/auth';
+import { usersService } from 'services/UsersService';
 
-export const useGetCategories = (options?: UseQueryOptions) => {
-  const { data: items, ...rest } = useQuery(['Cars'], categoryService.getAll, {
+export const useGetUsers = (options?: UseQueryOptions) => {
+  const { data: items, ...rest } = useQuery(['Users'], usersService.getAll, {
     staleTime: 1000 * 60 * 5, // 5 minutes
     onError: (error: AxiosError) => {
       toast.error(error.message);
     },
     ...options,
   });
-  const data = items as CarCategory;
+  const data = items as IUser[];
   return { data, ...rest };
 };
